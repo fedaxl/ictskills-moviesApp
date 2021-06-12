523432b9a256352e2b65983cdfb0bcc520c5364c
Federica Fiorenza <20091413@mail.wit.ie>
2021-06-12 01:52:20 +0100

    Home page components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.js"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
