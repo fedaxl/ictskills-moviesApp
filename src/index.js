8d8ae1c4f8571ad2789ff49fc0c0fe9c6cb51815
Federica Fiorenza <20091413@mail.wit.ie>
2021-06-14 23:28:40 +0100

    Full review page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import MovieReviewPage from "./pages/movieReviewPage";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
@@ -17,6 +18,7 @@ const App = () => {
       </li>
     </ul>
     <Switch>
+      <Route path="/reviews/:id" component={MovieReviewPage} />
       <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
       <Route path="/movies/:id" component={MoviePage} />
       <Route exact path="/" component={HomePage} />
